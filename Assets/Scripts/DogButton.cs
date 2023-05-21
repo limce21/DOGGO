c63db9281b9ead80c9fa504c09e072cdbf11f955
ChaenLim <limce21@naver.com>
2023-05-21 15:11:08 +0900

    feat: 버튼 클릭 시 텍스트 변경 구현

---
+++ Assets/Scripts/DogButton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DogButton : MonoBehaviour
+{
+    public string species; // 강아지 종류 정보
+    public Text speciesText; // 종류를 표시할 텍스트 UI
+    public Text inputNameText; // 입력된 이름을 표시할 텍스트 UI
+    public Image selectImg; // 선택된 이미지를 표시할 이미지 컴포넌트
+    public Sprite selectedSprite; // 선택된 상태의 스프라이트 이미지
+
+    public void OnButtonClick()
+    {
+        if (species == "husky")
+        {
+            speciesText.text = "허스키";
+            inputNameText.text = "흰 바탕에 뚜렷한 무늬가 특징!\n추위에 강하고 힘이 세요.\n사람을 좋아하고 장난기가 많아요.";
+        }
+        else if (species == "welshcorg")
+        {
+            speciesText.text = "웰시코기";
+            inputNameText.text = "짧은 다리에 뒤태는 치명적!\n활동적이고 적극적인 친구에요.\n스킨십을 좋아한답니다.";
+        }
+        else if (species == "retriever")
+        {
+            speciesText.text = "리트리버";
+            inputNameText.text = "웃는 얼굴이 천사 같은 강아지!\n충성심이 강하고 지능이 높아요.\n주인과 교감을 잘한답니다.";
+        }
+
+        // 선택된 이미지 업데이트
+        selectImg.sprite = selectedSprite;
+    }
+}
--- Assets/Scripts/StartSceneController.cs
+++ Assets/Scripts/StartSceneController.cs
@@ -1,31 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum Character
+{
+    Husky, WelshCorgin, Retriever
+}
+
 public class StartSceneController : MonoBehaviour
 {
     public GameObject introPanel;
     public GameObject selectPanel;
+    public static StartSceneController instance;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != null) return;
+        DontDestroyOnLoad(gameObject);
+    }
+
 
     private void Start()
     {
         // StartBtn 버튼에 클릭 이벤트를 등록합니다.
         Button startButton = GameObject.Find("StartBtn").GetComponent<Button>();
         startButton.onClick.AddListener(OnStartButtonClick);
-
-        if (selectPanel.activeSelf)
-        {
-            // WelshCorgiBtn 버튼을 클릭합니다.
-            Button welshCorgiButton = selectPanel.transform.Find("WelshCorgiBtn").GetComponent<Button>();
-            welshCorgiButton.onClick.Invoke();
-
-            // WelshCorgiBtn의 색상을 선택된 상태로 변경합니다.
-            welshCorgiButton.Select();
-
-            // WelshCorgiBtn의 색상을 선택 색상으로 변경합니다.
-            ColorBlock colors = welshCorgiButton.colors;
-            colors.normalColor = colors.selectedColor;
-            welshCorgiButton.colors = colors;
-        }
     }
 
     private void OnStartButtonClick()
@@ -36,4 +35,6 @@ public class StartSceneController : MonoBehaviour
         // SelectPanel을 활성화합니다.
         selectPanel.SetActive(true);
     }
+
+    public Character currentCharacter;
 }
